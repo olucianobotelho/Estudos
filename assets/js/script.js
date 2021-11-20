b5bda55046fdd92d03684b5780d9e8b5cc8421a4
Luciano Botelho <lucianopaiva.lpb@gmail.com>
2021-11-20 20:03:21 -0300

    abrir e fechar sacola implementados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,14 @@
-function openbag(){
-
+// Tradução do termo INNER = significa "Interno", innerhtml significa "Dentro do html"
+function openBag(){
+bag = document.querySelector('.cart-sidebar')
+bag.style.display="block"
 }
-
 function closebag(){
-
+bag.style.display="none"
 }
+document.getElementById('btn-cart').addEventListener('click',openBag)
+document.getElementById('btn-close-cart').addEventListener('click',closebag)
 
-function additem(){
 
-}
 
-function removeitem(){
 
-}
-
-function valuebadge(){
-
-}
