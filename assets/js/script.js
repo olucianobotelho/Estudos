6953ee5f6bc90589ecae60b58347265ab8f16760
Luciano Botelho <lucianopaiva.lpb@gmail.com>
2021-12-07 22:35:08 -0300

    implementando remover carrinho

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,36 +28,16 @@ document.getElementById('adicionar-item').addEventListener('click',closebag)
 
 
 const fetchProducts = () =>{
-    const data = document.querySelector('#data')
-
-    
-    
- fetch('http://127.0.0.1:5500/products.json')
-.then(response => response.json())
-.then(body => {
-
-    
-    
-    
-
-    
-        
-        
-   
+    const data = document.querySelector('#data')  
+    fetch('http://127.0.0.1:5500/products.json')
+    .then(response => response.json())
+    .then(body => {
     data.innerHTML = ''
-    
-
     body.groups.forEach(group=>{
-            
         let banana = `<section><h2>${group.name}</h2><div class="products-grid">`
-
             group.products.forEach( product =>{
-            
                 const description = product.description != null ? `<p>${product.description}` : ``
-
             banana+=
-            
-            
             `<article class="card">
                 <img src="${product.image}" alt="${product.name}" width="196" height="120">
                  <div class="card-content">
@@ -68,38 +48,28 @@ const fetchProducts = () =>{
                 data-id="${product.id}"
                 data-name="${product.name}"
                 data-image="${product.image}"
-                data-price="${product.price}"
-                
-                
+                data-price="${product.price}"                
                 ">Adicionar</button>
                  </div>
             </article>`
         })
             banana += '</div></section>'
             data.innerHTML+=banana
-            
+            })
+            setupAddToCart()     
     })
-
-    const removeOfCart = () =>{
-        console.log('Vou Remover')
-        const btnRemoveCartEls = document.querySelectorAll('#btn-remove')
-
-        
-    }
-    setupAddToCart()
-})
-.catch(err =>{
-console.log(err)
-data.innerHTML='<p class="alert-error"> Falha ao carregar produtos.Recarregue a Página</p>'
-
-})
+    .catch(err =>{
+        console.log(err)
+        data.innerHTML='<p class="alert-error"> Falha ao carregar produtos.Recarregue a Página</p>'  
+    })
+    
 }
-fetchProducts()
 
+fetchProducts()
 // PRODUCTS CART
 
 
-const productsCart = []
+let productsCart = []
 const addToCart = (event) => {
     const product = event.target.dataset
     const index = productsCart.findIndex((item) =>{
@@ -121,20 +91,47 @@ const addToCart = (event) => {
     }else{
         productsCart[index].qty++
     }
-handleCartUpdate()
+    handleCartUpdate()
+}
+
+
+
     
 
-}
-const setupAddToCart = () => {
-    const btnAddCartEls = document.querySelectorAll('.btn-add-cart')
-    console.log('btnAddCartEls', btnAddCartEls)
-    btnAddCartEls.forEach(btn => {
-        btn.addEventListener('click', addToCart)
-
-    })
-}
 
 
+
+
+    // ADICIONAR E REMOVER DO CARRINHO
+
+    function removeOfCart (){
+        const { id } = this.dataset
+        productsCart = productsCart.filter((product)=>{
+        if(product.id != id){
+            return true
+        }
+        return false
+        })
+        handleCartUpdate()
+       
+    }
+
+    const setupAddToCart = () => {
+        const btnAddCartEls = document.querySelectorAll('.btn-add-cart')
+        btnAddCartEls.forEach(btn => {
+            btn.addEventListener('click', addToCart)
+    
+        })
+    }
+
+    const setupRemoveOfCart = () => {
+        const btnRemoveCartEls = document.querySelectorAll('.btn-remove-cart')
+        btnRemoveCartEls.forEach((btn) => {
+            btn.addEventListener('click', removeOfCart)
+        })
+    }
+    
+    
 
 const handleCartUpdate = () =>{
    const badgeEl = document.querySelector('#btn-cart .badge') 
@@ -142,13 +139,9 @@ const handleCartUpdate = () =>{
    const cartWithProductsEl = document.querySelector('#cart-with-products') 
    const cartItensParent = cartWithProductsEl.querySelector('ul')
    const cartTotalValueEl = document.querySelector('#cart-total-value')
-
     const total = productsCart.reduce((total,item) =>{
         return total + item.qty
     }, 0)
-
-
-    
     if (total > 0 ){
         badgeEl.classList.add('badge-show')
         badgeEl.innerText = total
@@ -157,32 +150,21 @@ const handleCartUpdate = () =>{
         cartItensParent.innerHTML = ''
         productsCart.forEach((product)=>{
             cartItensParent.innerHTML += `<li class="cart-item"><img src="${product.image}" alt="" width="70" height="70">
-
             <div class="" id="${product.id}">
                 <p class="h3">${product.name}</p>
                 <p class="price">R$ ${product.price.toLocaleString('pt-br',{minimumFractionDigits:2})}</p>
             </div>
-            <input id="btn-remove" class="form-input" type="number" min="1" value="${product.qty}" name="quantidade" >
-            <button id="remover-item">
+            <input class="form-input" type="number" min="1" value="${product.qty}">
+            <button class="btn-remove-cart" data-id="${product.id}>
                 <i class="fas fa-trash-alt"></i>
             </button>
-
-
-
         </li>`
-
-
         })
-
+        setupRemoveOfCart()
         const totalPrice = productsCart.reduce((total, item)=>{
             return total + item.qty * item.price
-
-
-
-        },0)
-        
+        },0)    
         cartTotalValueEl.innerText = ('R$',totalPrice.toLocaleString('pt-br',{minimumFractionDigits:2}))
-
     } else{
         badgeEl.classList.remove('badge-show')
         emptyCartEl.classList.add('empty-cart-show')
@@ -190,7 +172,3 @@ const handleCartUpdate = () =>{
     }
 }
 handleCartUpdate()
-
-
-
-
